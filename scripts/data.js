87df7149a1a42efc5cd4c2540bbbd1c87cde9941
watsyurname529 <watsyurname529@yahoo.com>
2016-03-15 17:27:24 -0400

    Rearraged where data lives
    Moved the data for the generator into its own file.
    
    Added jQuery.min.js to the Vendor folder as an option.
    
    Minor edits to index.html

---
+++ scripts/data.js
@@ -0,0 +1,45 @@
+var data = {
+    heading: [
+       "It's a fucking",
+       "Check this shit out, a fucking"
+    ],
+    response: [
+       "Already fucking seen one",
+       "Give me a-fucking-nother one",
+       "Don't really give a fuck"
+    ],
+    template: [
+       "Big @color @animal",
+       "Silly @animal with @color fur"
+    ],
+    animal: [
+       "dog",
+       "cat",
+       "rabbit"
+    ],
+    color: [
+       "red",
+       "blue",
+       "green",
+       "yellow"
+    ]
+}
+
+// var key = '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc';
+// var path = 'https://spreadsheets.google.com/feeds/list/{key}/od6/public/values?alt=json';
+// var data = $.getJSON(path.replace('{key}', key));
+
+// var data, json;
+// $.getJSON('https://spreadsheets.google.com/feeds/list/0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc/od6/public/values?alt=json', function(json){
+//     data = jQuery.parseJSON(json);
+// });
+
+// $.ajax({
+//     url: path.replace( '{key}', key ),
+//     success: function( json, status, xhr ) {
+//         data = json;
+//     },
+//     error: function( xhr, errorType, error ) {
+//         throw 'Cannot load spreadsheet. Is it published? (@see https://support.google.com/drive/answer/37579?hl=en)';
+//     }
+// });
--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,30 +1,3 @@
-data = {
-    heading: [
-       "It's a fucking",
-       "Check this shit out, a fucking"
-    ],
-    response: [
-       "Already fucking seen one",
-       "Give me a-fucking-nother one",
-       "Don't really give a fuck"
-    ],
-    template: [
-       "Big @color @animal",
-       "Silly @animal with @color fur"
-    ],
-    animal: [
-       "dog",
-       "cat",
-       "rabbit"
-    ],
-    color: [
-       "red",
-       "blue",
-       "green",
-       "yellow"
-    ]
-}
-
 generate = () ->
     heading_string = data['heading'][Math.floor(Math.random() * data['heading'].length)]
     generate_string = data['response'][Math.floor(Math.random() * data['response'].length)]
